a08321434f5ede8958972f469acefc9a5f763f87
Aldo-kriek <118925007+Aldo-kriek@users.noreply.github.com>
2023-03-08 21:56:11 +0200

    Add files via upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+import java.util.*;
+import java.io.*;
+
+/**
+ *
+ * @author Studio
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) throws FileNotFoundException {
+       frmMain frmMain = new frmMain();
+               frmMain.show();
+    
+        
+    }
+   
+    
+    
+    
+}
